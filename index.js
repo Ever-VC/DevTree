caedec08006e233b7ae5175e66f445e6b32121ec
Ever-VC <ever.vasquezc27@gmail.com>
2024-12-31 13:54:34 -0600

    💥 NEW: Hola Mundo en Express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello, world!');
+const express = require('express');
+
+const app = express();
+
+// Routing
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000...');
+});
