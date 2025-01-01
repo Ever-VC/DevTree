7657f183647fff03cf413010a09f0cbe3e1e084a
Ever-VC <ever.vasquezc27@gmail.com>
2024-12-31 23:22:19 -0600

    📦 Configuration:  Dependencia para diseñar con colores las salidas de mensajes a consola

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
+import colors from 'colors';
 
 export const connect = async () => {
     try {
         const {connection} = await mongoose.connect(process.env.MONGODB_URI);
         const url = `${connection.host}:${connection.port}/${connection.name}`;
-        console.log('DB Connected at: ', url);
+        console.log(colors.cyan.bold(`DB Connected at: ${url}`));
     } catch (error) {
-        console.log('Error connecting to DB: ', error.message);
+        console.log(colors.bgRed.white.bold(error.message));
         process.exit(1);
     }
 };
