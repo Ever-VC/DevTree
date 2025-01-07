f676cdd13815e1cabfef1310bb390f36fee8d5c8
Ever-VC <ever.vasquezc27@gmail.com>
2025-01-06 21:00:17 -0600

    💥 NEW: Creando un habndler (nombre de usuario) único con la dependencia slug

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import slug from "slug";
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
 
@@ -13,8 +14,18 @@ export const createAcount = async(req: Request, res: Response) => {
         return res.status(409).json({error: error.message});
     }
 
+    const handle = slug(req.body.handle, '');
+    const handleExists = await User.findOne({handle});
+
+    if(handleExists) {
+        const error = new Error('Nombre de usuario no disponible.');
+        return res.status(409).json({error: error.message});
+    }
+
     const user = new User(req.body);
     user.password = await hashPassword(password);
+    user.handle = handle;
+
     await user.save();
     res.status(201).send('Registro creado correctamente.');
 }
