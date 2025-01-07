4a395d83f16ea648bb22fbf9c87e9cdcc32a0474
Ever-VC <ever.vasquezc27@gmail.com>
2025-01-06 21:23:41 -0600

    💥 NEW: Validando los campos requeridos

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
 import slug from "slug";
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
 
 export const createAcount = async(req: Request, res: Response) => {
 
+    // Manejar errores de validación
+    let errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
     const {email, password} = req.body;
 
     const userExists = await User.findOne({email});
 
     if(userExists) {
-        const error = new Error('El correo ya está registrado.');
+        const error = new Error('El correo ya esta registrado.');
         return res.status(409).json({error: error.message});
     }
 
@@ -18,7 +25,7 @@ export const createAcount = async(req: Request, res: Response) => {
     const handleExists = await User.findOne({handle});
 
     if(handleExists) {
-        const error = new Error('Nombre de usuario no disponible.');
+        const error = new Error('El handle ya esta en uso.');
         return res.status(409).json({error: error.message});
     }
 
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,24 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { createAcount } from "./handlers";
 
 const router = Router();
 
 // Autenticación y registro
-router.post('/auth/register', createAcount);
+router.post('/auth/register', 
+    body('handle')
+        .notEmpty()
+        .withMessage('El handle es requerido.'),
+    body('name')
+        .notEmpty()
+        .withMessage('El nombre de usuario es requerido.'),
+    body('email')
+        .isEmail()
+        .withMessage('El email no es álido.'),
+    body('password')
+        .isLength({min: 8})
+        .withMessage('El password no es valido, debe contener como minimo {8} caracteres.'),
+    createAcount
+);
 
 export default router;
