07199dceb32a94074aa141f46a9bdf7d6abb5ddf
Ever-VC <ever.vasquezc27@gmail.com>
2025-01-15 21:47:14 -0600

    💥 NEW:

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
-//const express = require('express'); // CommonJS CJS
 import express from 'express'; // ES6 Module ESM (ECMAScript Module)
+import cors from 'cors';
 import 'dotenv/config';
 import router from './router';
 import { connect } from './config/db';
+import { corsConfig } from './config/cors';
+
+connect();
 
 const app = express();
-connect();
+
+// Cors
+app.use(cors(corsConfig));
 
 // Leer el body de las peticiones
 app.use(express.json());
