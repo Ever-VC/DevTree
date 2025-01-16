1366c0cbaed7bbe6bdc87335db52cce992d017c1
Ever-VC <ever.vasquezc27@gmail.com>
2025-01-15 21:48:31 -0600

    Revert "💥 NEW:"
    This reverts commit 07199dceb32a94074aa141f46a9bdf7d6abb5ddf.

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
+//const express = require('express'); // CommonJS CJS
 import express from 'express'; // ES6 Module ESM (ECMAScript Module)
-import cors from 'cors';
 import 'dotenv/config';
 import router from './router';
 import { connect } from './config/db';
-import { corsConfig } from './config/cors';
-
-connect();
 
 const app = express();
-
-// Cors
-app.use(cors(corsConfig));
+connect();
 
 // Leer el body de las peticiones
 app.use(express.json());
