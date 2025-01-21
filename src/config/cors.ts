049fb176699e9d8acf21743143f4541f163709d0
Ever-VC <ever.vasquezc27@gmail.com>
2025-01-20 19:35:08 -0600

    💥 NEW: Generando script que permita resolver peticiones desde postman o una herramienta similar para pruebas en desarrollo

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,14 @@ import { CorsOptions } from "cors";
 
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback) {
-        if (origin === process.env.FRONTEND_URL || origin === process.env.FRONTEND_URL_LOCAL) {
+        const whitelist = [process.env.FRONTEND_URL, process.env.FRONTEND_URL_LOCAL];
+
+        if (process.argv.includes('--api')) {
+            whitelist.push(undefined);
+        }
+
+        //if (origin === process.env.FRONTEND_URL || origin === process.env.FRONTEND_URL_LOCAL) {
+        if (whitelist.includes(origin)) {
             callback(null, true);
         } else {
             callback(new Error('Error de CORS'));
